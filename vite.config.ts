f10d5035de7d04d312dadc83d5ba33c8de495a0f
Mahmoud <mahmoudft8@gmail.com>
2023-12-07 09:01:27 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -11,32 +11,6 @@ export default defineConfig({
                 // https://sharp.pixelplumbing.com/api-output#png
                 quality: 30,
             },
-            svg: {
-                multipass: true,
-                plugins: [
-                    {
-                        name: "preset-default",
-                        params: {
-                            overrides: {
-                                cleanupNumericValues: false,
-                                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
-                            },
-                            cleanupIDs: {
-                                minify: true,
-                                remove: false,
-                            },
-                            convertPathData: false,
-                        },
-                    },
-                    "sortAttrs",
-                    {
-                        name: "addAttributesToSVGElement",
-                        params: {
-                            attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-                        },
-                    },
-                ],
-            },
         }),
     ],
 });
