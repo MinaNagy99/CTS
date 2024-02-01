a863f28c4d028de7206e5e9d1ae93742dad80e03
MinaNagy99 <minanagykhalefa@gmail.com>
2024-02-01 10:42:19 +0200

    a

--- src/components/Blog/BreadCrumbs.tsx
+++ src/components/Blog/BreadCrumbs.tsx
@@ -1,14 +1,19 @@
 import { Trans } from 'react-i18next';
 import { Link } from 'react-router-dom';
+interface propsType {
+    data: {
+        name: string;
+        link: string;
+    };
+}
 
-const BreadCrumbs = ({ data }) => {
-
+const BreadCrumbs = ({ data }: propsType) => {
     return (
         <div className="d-flex">
             {data.map((item, index) => (
                 <div key={index} className="">
                     <Link className="text-secondary text-decoration-none" to={item.link}>
-                        {<Trans i18nKey={item.name.substring(0,15)}></Trans>}
+                        {<Trans i18nKey={item.name.substring(0, 15)}></Trans>}
                     </Link>
                     {index !== data.length - 1 && <span className="px-3">/</span>}
                 </div>
